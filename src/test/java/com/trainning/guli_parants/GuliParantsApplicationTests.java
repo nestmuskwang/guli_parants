694f4d735242172b9877a3f37a3366000133cdcf
WangShuguang <wangshuguang@e-sscard.com>
2021-09-21 18:02:13 +0800

    Initial commit

---
+++ src/test/java/com/trainning/guli_parants/GuliParantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainning.guli_parants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliParantsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
